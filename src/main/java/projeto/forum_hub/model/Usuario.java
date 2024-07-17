c52f22654086c59edc2add029f57c88560cfdf79
Welder <welder.carlos53@gmail.com>
2024-07-16 22:59:51 -0300

    v1 começo do model

---
+++ src/main/java/projeto/forum_hub/model/Usuario.java
@@ -0,0 +1,21 @@
+package projeto.forum_hub.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @ManyToOne
+    private Perfil perfil;
+
+}
